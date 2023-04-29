626cbf54d146983d185b6a826455069f68c42c7a
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-04-29 21:24:21 +0700

    change design component header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Flex, Heading} from "@chakra-ui/react";
+import { Flex, Heading, Box} from "@chakra-ui/react";
 
 const Header = () => (
-  <Flex align={'center'} justify='center' height='350px' minWidth={'100%'} padding='20px 50px' >
-      <Flex className="header-bg" flexDirection={'column'} w={'100%'} h='100%' padding='0 50px' align={{
-        base: "center",
-        xl: 'flex-start',
-        lg: 'flex-start',
-        md: 'center'
-      }} justify={'center'}>
-        <Heading as={'h1'} fontSize='7xl' color='blue.50' backgroundColor='#395B64' p={'0 15px'} m='5px 0'>Realtor</Heading>
-        <Heading as={'h2'} fontSize={'2xl'} color={'#395B64'} background='blue.50' padding={'3px 15px'} >Best Deals for Buying and Renting Homes</Heading>
+  <Flex align='center' justify='center' height='500px' minWidth={'100%'} p='25px 0px'>
+      <Flex w='50%' justify='center' align='center' textAlign='left' flexDirection='column'>
+        <Box p='20'>
+          <h1 className='header-title'>Come see the homes </h1>
+          <h1 className='header-title'>We <span>rent</span> and <span>sell</span>.</h1>
+          <p color='#CBD5E0'>realtors provide services for those of you who want to buy or rent a housing need</p>
+        </Box>
       </Flex>
+      <Box className='header-bg' borderRadius='150px 0px 0px 0px'>
+
+      </Box>
   </Flex>  
 );
 
